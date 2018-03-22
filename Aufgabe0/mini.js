55cf97d80880cc2b19f624679626e0699891f009
apple <apple@apples-mbp.eduroam.hs-furtwangen.de>
2018-03-22 13:45:36 +0100

    Mini-Seite hochgeladen

---
+++ Aufgabe0/mini.js
@@ -0,0 +1,8 @@
+function Eingabefeld() {
+    var eingabe = prompt("Hey, wie heißt du?", "");
+    if (eingabe != null) {
+        document.getElementById("Type").innerHTML =
+            "cooler Name, " + eingabe + ". vielleicht nenne ich mein Kind auch mal so.";
+    }
+}
+//# sourceMappingURL=mini.js.map
---
+++ Aufgabe0/mini.ts
@@ -0,0 +1,7 @@
+function Eingabefeld() {
+  var eingabe = prompt("Hey, wie heißt du?", "");
+  if (eingabe != null) {
+    document.getElementById("Type").innerHTML =
+      "cooler Name, " + eingabe + ". vielleicht nenne ich mein Kind auch mal so.";
+  }
+}
