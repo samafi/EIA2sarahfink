cc8ca74a0f9d7896607d6d736dfd3e97a6dc5626
apple <apple@apples-mbp.fritz.box>
2018-03-26 20:12:58 +0200

    .

--- Aufgabe0/mini.ts
+++ Aufgabe0/mini.ts
@@ -1,5 +1,5 @@
-function Eingabefeld() {
-  var eingabe = prompt("Hey, wie heißt du?", "");
+function Eingabefeld(): void {
+  var eingabe: string = prompt("Hey, wie heißt du?", "");
   if (eingabe != null) {
     document.getElementById("Type").innerHTML =
       "cooler Name, " + eingabe + ". vielleicht nenne ich mein Kind auch mal so.";
