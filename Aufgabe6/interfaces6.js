1622cfdfbde5f6adaeae758caff0bc7e1f8210cf
apple <apple@iphone5onthomas.localdomain>
2018-05-26 19:38:16 +0200

    .

---
+++ Aufgabe6/interfaces6.js
@@ -0,0 +1,6 @@
+var Aufgabe6;
+(function (Aufgabe6) {
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    Aufgabe6.studiHomoAssoc = {};
+})(Aufgabe6 || (Aufgabe6 = {}));
+//# sourceMappingURL=interfaces6.js.map
---
+++ Aufgabe6/interfaces6.ts
@@ -0,0 +1,18 @@
+namespace Aufgabe6 {
+    // Struktur des heterogenen assoziativen Arrays als Datensatz für eine studierende Person
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        studiengang: string;
+    }
+
+    // Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+    export interface Studis {         [matrikel: string]: Studi;
+    }
+ 
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    export let studiHomoAssoc: Studis = {};  
+}
