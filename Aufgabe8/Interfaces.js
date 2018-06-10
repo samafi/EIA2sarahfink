1debb9dc0e772bc4af9823fc99bdc818636affc1
apple <apple@apples-mbp.fritz.box>
2018-06-10 12:13:51 +0200

    .

---
+++ Aufgabe8/Interfaces.js
@@ -0,0 +1,6 @@
+var Aufgabe8;
+(function (Aufgabe8) {
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    Aufgabe8.studiHomoAssoc = {};
+})(Aufgabe8 || (Aufgabe8 = {}));
+//# sourceMappingURL=Interfaces.js.map
---
+++ Aufgabe8/ProcessForm.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=ProcessForm.js.map
